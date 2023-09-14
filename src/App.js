90df28f0cf397a809376634c4be30a8702fadad3
Lhemingw <lhemingw@gmail.com>
2023-09-13 21:06:45 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+// import Search from "./Search";
 
-function App() {
+// import API_KEY from './config';
+import './App.css';
+ import './index';
+
+ const App = () => {
+  const [searchResults] = useState([]); //took out setSearchResults
+  const [movies, setMovies] = useState([]);
+  //  const [selectedMovie, setSelectedMovie] = useState(null);
+// Define a function to handle the search
+const handleSearch = (searchValue) => {
+  // Perform your search logic here and update searchResults state
+  // For example, you can fetch data from an API and update searchResults
+  // setSearchResults([...newSearchResults]);
+}
+
+
+
+  useEffect(() => {
+    // Fetch a list of popular movies from the API when the component mounts
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=45f74583`)
+       .then((response) => response.json())
+       .then((data) => setMovies(data.results))
+      //  if (data.Search) {
+      //   // If the API response contains a 'Search' property, it's an array of movies
+      //   setMovies(data.Search);
+      // } else {
+      //   console.error('Error fetching movies:', data.Error);
+      //}
+    //})
+       .catch((error) => console.error('Error fetching movies:', error));
+   }, []);
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Popular Movies</h1>   
       </header>
+      {/* Include the Search component */}
+      {/* <Search search={handleSearch} /> */}
+
+      {/* Render search results */}
+      <div className="search-results">
+        {/* Render search results here */}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
+//       // {/* Render movie components, review forms, etc. */}
+  
+
+
+
+
+
+ 
+
+
---
+++ src/config.js
@@ -0,0 +1,4 @@
+// src/config.js
+const API_KEY = 'http://www.omdbapi.com/?i=tt3896198&apikey=45f74583';
+
+export default API_KEY;
